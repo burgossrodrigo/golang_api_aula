fbf8476c080d36a1748e456753740dcab9575ac1
Rodrigo Burgos <rodrigoburgos@live.com>
2024-12-26 16:16:36 -0300

    post route

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ go mod init golang-api
 ```bash
 go get github.com/gin-gonic/gin
 ```
+
+## Chamando a api
+
+```bash
+curl http://localhost:8080/v1/horas
+```
+
+## Chamada de api com educação
+
+```bash
+curl -X POST http://localhost:8080/v1/horas -H "Content-Type: application/json" -d '{"mensagem":"por_favor"}'
+```
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"golang-api/pkg/models"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,4 +11,37 @@ func main() {
 	r := gin.Default()
 
 	eg := r.Group("/v1")
+
+	{
+		eg.GET("/horas", horas)
+		eg.POST("/horas", horasComEducação)
+	}
+
+	r.Run(":8080")
+}
+
+func horas(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"horas": time.Now().Format("15:04:05"),
+	})
+}
+
+func horasComEducação(c *gin.Context) {
+
+	var mensagemModel models.HorasMensagem
+
+	if err := c.ShouldBindJSON(&mensagemModel); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+	}
+
+	if c.Param("mensagem") != "por_favor" {
+		c.JSON(200, gin.H{
+			"horas":    time.Now().Format("15:04:05"),
+			"mensagem": "Parabéns! Você é educado!",
+		})
+	} else {
+		c.JSON(400, gin.H{
+			"error": "palavra mágica?",
+		})
+	}
 }
